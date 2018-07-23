6ff07d7eb1807589b5d8acddcfd103dc9df40e00
Robin Lamberti <lamberti.robin@gmail.com>
2018-07-23 22:38:37 +0200

    added convenience param assignment for createMamFrom

--- test/testMock.js
+++ test/testMock.js
@@ -9,12 +9,21 @@ describe("testing mocked MAM client", async () => {
         const client = await mamClient.createMamFrom({
             mode: 'private'
         })
-        await mamClient.publish("Test", client.mam, client.iota, false)
+        await mamClient.publish("Test", client.mam, client.iota)
         await mamClient.createMam()
-        await mamClient.publish("Zweiter Test", client.mam, client.iota, false)
+        await mamClient.publish("Zweiter Test", client.mam, client.iota)
         const client2 = await mamClient.createMam(client.mam.seed, null, 'private')
-        const r = await mamClient.getMessages(client.channelRoot, 'private')
+        const r = await mamClient.getMessages(client2.channelRoot, 'private')
         console.log("Messages:", r.messages)
         expect(r.messages).to.deep.equal(["Test", "Zweiter Test"])
+    })
+
+    it("should return constant root as long as no messages are attached", async () => {
+        const client = await mamClient.createMamFrom()
+        const r = mamClient.getRoot(client.mam)
+        //next root of channel should be the same as long as no new message was published
+        expect(mamClient.getRoot(client.mam)).to.equal(r)
+        await mamClient.publish("Test", client.mam, client.iota)
+        expect(mamClient.getRoot(client.mam)).to.not.equal(r)
     })
 })
